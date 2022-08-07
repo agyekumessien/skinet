f8beeeabcd5a82abf6527db1e2c9a3ac0cd6dac0
Peter Essien <agyekumessien@yahoo.com>
2022-08-06 21:26:07 -0400

    End of Section 5

--- API/Controllers/WeatherForecastController.cs
+++ API/Controllers/WeatherForecastController.cs
@@ -1,9 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
+    [ApiExplorerSettings(IgnoreApi = true)]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
